02373687bef30c745090337fbe1055bc3e8794aa
Ay <ay.kerem@training.lug-ag.de>
2024-12-09 10:07:33 +0100

    first commit

---
+++ Korrektur.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persons
+{
+    internal class Korrektur
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo User! Bitte gib einen Namen ein.");
+            string userName = Console.ReadLine();
+
+            Person person1 = new Person("Maria", 31, "Metzgerin");
+            Person person2 = new Person("Rainer", 40, "Programmierer");
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine($"Wähle eine Person aus (1 - 2):\n\n1.{person1.Name}\n2.{person2.Name}");
+                string input Console.ReadLine();
+
+                if(input == "1")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Soll die person dich begrüßen, oder sollen die Information der Person ausgegeben werden? (1 - 2):\n1.Begrüßung\n2.Infos");
+                    input = Console.ReadLine();
+
+                    if(input == "1")
+                    {
+                        Console.Clear();
+                        person1.Begruessung(userName);
+                        Console.ReadKey();
+                    }
+                    else if(input == "2")
+                    {
+                        Console.Clear();
+                        person1.Infos(userName);
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Deine Eingabe ist fehlerhaft!");
+                        Console.ReadKey();
+                    }
+                }
+                else if(input == "2")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Soll die person dich begrüßen, oder sollen die Information der Person ausgegeben werden? (1 - 2):\n1.Begrüßung\n2.Infos");
+                    input = Console.ReadLine();
+
+                    if (input == "1")
+                    {
+                        Console.Clear();
+                        person2.Begruessung(userName);
+                        Console.ReadKey();
+                    }
+                    else if (input == "2")
+                    {
+                        Console.Clear();
+                        person2.Infos(userName);
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Deine Eingabe ist fehlerhaft!");
+                        Console.ReadKey();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Deine Eingabe war nicht korrekt!");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            }
+        }
+    }
+}
+// In C# (und auch anderen Sprachen) kann statt mit Get- und Set- Methoden auch mit Eigenschaften gearbeitet werden.
+// Dies erleichtert Verwendung und Zugriff.
+// Um die Eigenschaften zu erstellen, makiert man alle Felder, führt einen Rechtsklick aus
+// und wählt dann "Schnellaktionen und Refractorings" aus
+// Dann "Konstruktor" und dann das gleiche nochmal und "Felder Kapseln (jedoch weiterhin verwenden)"
+//Um die Eigenschaften zu erstellen , makiert man alle Felder, führt einen Rechtsklick aus
+//und wählt dann "Schnellaktionen und Refactorings" aus.
+//Dann "Konstruktor" und dann das gleiche nochmal und "Felder Kapseln (jedoch weiterhin verwenden)"
+//Im Grunde ersetzten die Eigenschaften einfach die Methoden mit Get und Set und das in einer deutlich kürzeren Schreibweise
+
---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿namespace Persons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bitte geben Sie Ihren Namen ein:");
+            string userName = Console.ReadLine();
+
+            Person person1 = new Person("Heinrich Keller", 30, "Softwareentwickler");
+            Person person2 = new Person("Lina Reiner", 28, "Lehrerin");
+
+            bool running = true;
+            while (running)
+            {
+                Console.Clear();
+                Console.WriteLine("Wählen Sie eine Option:");
+                Console.WriteLine("1. Person 1 begrüßen");
+                Console.WriteLine("2. Person 2 begrüßen");
+                Console.WriteLine("3. Infos zu Person 1 anzeigen");
+                Console.WriteLine("4. Infos zu Person 2 anzeigen");
+                Console.WriteLine("5. Beenden");
+                Console.Write("Ihre Auswahl: ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        person1.Begruessung(userName);
+                        break;
+                    case "2":
+                        person2.Begruessung(userName);
+                        break;
+                    case "3":
+                        person1.Infos();
+                        break;
+                    case "4":
+                        person2.Infos();
+                        break;
+                    case "5":
+                        running = false;
+                        break;
+                    default:
+                        Console.WriteLine("Ungültige Auswahl. Bitte versuchen Sie es erneut.");
+                        break;
+                }
+
+                if (choice != "5")
+                {
+                    Console.WriteLine("\nDrücken Sie eine beliebige Taste, um fortzufahren...");
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+        public int Alter { get; set; }
+        public string Beruf { get; set; }
+
+        public Person(string name, int alter, string beruf)
+        {
+            Name = name;
+            Alter = alter;
+            Beruf = beruf;
+        }
+        public void Begruessung(string userName)
+        {
+            Console.WriteLine($"Hallo {userName}, mein Name ist {Name}. Ich bin {Alter} Jahre alt und arbeite als {Beruf}.");
+        }
+
+        public void Infos()
+        {
+            Console.WriteLine($"Name: {Name}, Alter: {Alter}, Beruf: {Beruf}");
+        }
+    }
+}
